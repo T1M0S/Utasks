1f2c1fbda86ea401b944679a8a2d36315532a981
T1M0S <kutkin.roma@bk.ru>
2024-01-18 01:34:09 +0300

    task1

---
+++ pythonProject/tasks/task1.py
@@ -0,0 +1,7 @@
+full_name = input("Введите вашу фамилию, имя: ")
+age = input("Введите ваш возраст: ")
+birth_place = input("Введите ваше место рождения: ")
+
+print("Ваши фамилия имя: " + full_name)
+print("Ваш возраст: " + age)
+print("Вы живете в городе " + birth_place)
